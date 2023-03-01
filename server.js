0762d6f8d4e277c20fcbe0c618775d8e7bdadfcb
MLD4 <dongmiao123@gmail.com>
2023-03-02 08:01:47 +1030

    html routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.json()); 
 
 app.get('/', (req, res) => -
     res.sendFile(path.join(__dirname, './public/index.html'))
